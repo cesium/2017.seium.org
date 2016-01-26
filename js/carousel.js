cfb2febeb2516bdb8b256bbf324dcc6fe9b65ada
Martinho Aragão <martinhoaragao@gmail.com>
2016-01-26 23:01:32 +0000

    Add carousel, refactor partner section in heartbits. Bugged in homepage

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,6 @@
 $(document).ready(function(){
   $('.heartbits-partners-carousel').slick({
     autoplay: true,
-    slidesToShow: 2
+    slidesToShow: 3
   });
-
-  $('.project-body').hide();
-
-            $('.project-link').click(function(e){
-                 e.preventDefault();
-                 var $this = $(this).parent().find('.project-body');
-                 $(".project-body").not($this).hide();
-                 $(".project").not($(this).parent()).removeClass("project-border");
-                 $($this).toggle();
-                 $(this).parent().toggleClass("project-border");
-
-                 $(window).trigger('resize');
-
-                 $("body, html").animate({
-                     scrollTop: $( $(this).attr('href') ).offset().top
-                });
-            });
 });
