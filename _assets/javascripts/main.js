3c9067d02dc9ddd21e5dbdf0a66ceae0e491f6ad
Martinho Aragão <martinhoaragao@gmail.com>
2016-02-09 16:24:31 +0000

    Improve collapse, remove map js errors

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -4,9 +4,6 @@
 //= require vendor/slick.js
 //= require carousel.js
 
-//= require vendor/map-api.js
-//= require map.js
-
 //= require nav-var.js
 //= require about-text.js
 //= require scrolldiv.js
