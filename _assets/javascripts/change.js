4a5e83378739881a93bdfc5987669f487b30a6b1
Simbs38 <a75625@alunos.uminho.pt>
2017-01-25 21:08:49 +0000

    added speakers about, read more/read less

---
+++ _assets/javascripts/change.js
@@ -0,0 +1,8 @@
+function change(n){
+	if(document.getElementById("but"+n).value==="Read Less.."){
+		document.getElementById("but"+n).value="Read More..";	
+	}
+	else{
+		document.getElementById("but"+n).value="Read Less..";
+	}
+}
