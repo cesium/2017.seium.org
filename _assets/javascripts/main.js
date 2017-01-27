840a14cc5e464afc2853fe4b0a245c5c17b2d96b
Simbs38 <a75625@alunos.uminho.pt>
2017-01-27 16:31:33 +0000

    moddals working and 3 speakers by row applied

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -4,8 +4,13 @@
 //= require vendor/slick.js
 //= require carousel.js
 
+
+//= require vendor/animatedModal.min
+//= require vendor/animatedModal
+
 //= require nav-var.js
 //= require about-text.js
 //= require scrolldiv.js
 //= require countdown.js
 //= require change.js
+//= require animatedModal.js	
