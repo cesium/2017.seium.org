425aa926a690868b3ae2813436c21748056cdcc1
Simbs38 <a75625@alunos.uminho.pt>
2017-01-29 02:02:25 +0000

    updated main.js

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -3,13 +3,7 @@
 
 //= require vendor/slick.js
 
-
-//= require vendor/animatedModal.min
-//= require vendor/animatedModal
-
 //= require nav-var.js
 //= require about-text.js
 //= require scrolldiv.js
 //= require countdown.js
-//= require change.js
-//= require animatedModal.js	
