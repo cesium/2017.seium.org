4e4a92cb2bfc21b3e2700f3720205f51c84381bc
Simbs38 <a75625@alunos.uminho.pt>
2017-01-29 02:03:52 +0000

    remove trash

--- _assets/javascripts/change.js
+++
@@ -1,8 +0,0 @@
-function change(n){
-	if(document.getElementById("but"+n).value==="Read Less.."){
-		document.getElementById("but"+n).value="Read More..";	
-	}
-	else{
-		document.getElementById("but"+n).value="Read Less..";
-	}
-}
